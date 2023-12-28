29bc86ac43ed4c571e161b0e653866df00aa3e66
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2023-12-28 23:21:51 +0200

    Update admin.py

--- app_management/admin.py
+++ app_management/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Province, Store,Store_Map,StorePerformance,EntranceEvaluation,Store_level,Employee,Inside,Outside,Menu,McCafe,Drivethru,Delivery
 
 admin.site.register(Province)
+admin.site.register(Store_Map)
 admin.site.register(Store_level)
 admin.site.register(Employee)
 admin.site.register(Inside)
