a7df41bdcb0e283a0fe1f15a332952942662110a
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2024-01-24 18:02:10 +0200

    Update admin.py

--- app_management/admin.py
+++ app_management/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
+# Register your models here.
+from .models import Shops as Store_level,Inside,Outside,Menu,Mccafe as McCafe,Drivethru as Drivethru,Delivery as Delivery
 
+admin.site.register(Store_level)
+admin.site.register(Inside)
+admin.site.register(Outside)
+admin.site.register(Menu)
+admin.site.register(McCafe)
+admin.site.register(Drivethru)
+admin.site.register(Delivery)
