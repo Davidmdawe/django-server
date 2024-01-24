5b3f7f286bd7ec514917b9f9e786308650fe71b2
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2024-01-24 11:42:12 +0200

    Update admin.py

--- app_management/admin.py
+++ app_management/admin.py
@@ -1,15 +1,2 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Province, Store,Store_Map,StorePerformance,EntranceEvaluation,Store_level,Employee,Inside,Outside,Menu,McCafe,Drivethru,Delivery
-
-admin.site.register(Province)
-admin.site.register(Store_Map)
-admin.site.register(Store_level)
-admin.site.register(Employee)
-admin.site.register(Inside)
-admin.site.register(Outside)
-admin.site.register(Menu)
-admin.site.register(McCafe)
-admin.site.register(Drivethru)
-admin.site.register(Delivery)
