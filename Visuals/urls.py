09b504fc4adcbefa2e80e398264c97d950094c52
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2024-01-26 15:49:00 +0200

    Add files via upload

--- Visuals/urls.py
+++ Visuals/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path, re_path
-from app_management.views import home_view,login_view,get_store_data,MapDataView,get_store_data_store_level  # Import your home view
+from app_management.views import home_view,login_view,get_store_data,MapDataView,get_store_data_store_level,visuals_view  # Import your home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app_management.urls')),
     
     path('', login_view, name='login'),
+    path('visuals/', visuals_view, name='visuals'),
     path('McDonalds_Retail_Audit_Reporting/', home_view, name='home'),  # Add this line to handle the root path
     path('get_store_data/', get_store_data, name='get_store_data'),
     path('get_store_data_store_level/', get_store_data_store_level, name='get_store_data_store_level'),
