fa6293e6340713417b78885933a215ac38d15359
Davidmdawe <53513633+Davidmdawe@users.noreply.github.com>
2024-02-15 20:45:54 +0200

    Update urls.py

--- app_management/urls.py
+++ app_management/urls.py
@@ -1,12 +1,13 @@
-
 from django.urls import path
-from .views import login_view, home_view,logout_view,get_store_data,MapDataView,get_store_data_store_level,visuals_view
+from .views import login_view, home_view,logout_view,get_store_data,MapDataView,get_store_data_store_level,visuals_view,get_provinces,get_stores
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('visuals/', visuals_view, name='visuals'),
     path('logout/', logout_view, name='logout'),
+    path('get-provinces/',get_provinces , name='get_provinces'),
+    path('get-stores/',get_stores , name='get_stores'),
     path('get_store_data_store_level/', get_store_data_store_level, name='get_store_data_store_level'),
     path('get_store_data/', get_store_data, name='get_store_data'),
     path('get_map_data/', MapDataView.as_view(), name='get_map_data'),
